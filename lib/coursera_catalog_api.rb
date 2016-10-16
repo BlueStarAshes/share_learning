daf06577a0f0118797dc853ffc77781fd0262be0
Hsing-Yu, Chen <andy19933@gmail.com>
2016-10-16 23:58:27 +0800

    First refactor

--- lib/coursera_catalog_api.rb
+++ lib/coursera_catalog_api.rb
@@ -1,65 +1,84 @@
 require 'http'
 require 'json'
 
-# Create an API response dump
-coursera_response = {}
+# This class is in charge of the work involving using Coursera API
+class CourseraCatlogApi
+  COURSERA_CATOLOG_API_URL = 'https://api.coursera.org/api/courses.v1'.freeze
+  BATCH_SIZE = 100
 
-# Ask user to input a query word
-puts 'Input the desired course number to start retrieving from:'
-course_start_num = gets.chomp!.to_i
-puts 'Input the desired number of courses to retrieve:'
-course_num_limit = gets.chomp!.to_i
+  attr_reader :total_course_num, :courses
 
-# Retrieve the courses batch by batch
-total_course_num_unset = true
-total_course_num = -1
-batch_size = 100
-number_of_batches = course_num_limit / batch_size
-number_of_batches += 1 unless (course_num_limit % batch_size).zero?
-number_of_batches.times do
-  # Jump out the loop if we know there is not any courses left
-  unless total_course_num_unset
-    break if course_start_num >= total_course_num
+  def initialize
+    # Create an API response dump
+    @coursera_response = {}
+    # Create an dump for all courses
+    @courses = {}
+    retrieve_total_course_num
+    retrieve_all_courses_info
+    print_all_courses
   end
 
-  # Use the API to get courses response on Coursera
-  query_response =
-    HTTP.get('https://api.coursera.org/api/courses.v1?start=300&limit=10',
-             params: { start: course_start_num,
-                       limit: course_num_limit })
-  body_str = query_response.body.to_s
-  coursera_response[:elements] = JSON.parse(body_str)['elements']
-  coursera_response[:paging] = JSON.parse(body_str)['paging']
-  coursera_response[:linked] = JSON.parse(body_str)['linked']
-
-  # Adjust the course number limit according to the total number of courses
-  # reponded by Coursera
-  if total_course_num_unset
-    total_course_num = coursera_response[:paging]['total']
-    total_course_num_unset = false
+  def retrieve_total_course_num
+    # Retrieve the total number of courses on the catlog
+    course_start_num = 0
+    course_num_limit = 0
+    query_response =
+      HTTP.get(COURSERA_CATOLOG_API_URL,
+               params: { start: course_start_num,
+                         limit: course_num_limit })
+    body_str = query_response.body.to_s
+    @coursera_response[:paging] = JSON.parse(body_str)['paging']
+    @total_course_num = @coursera_response[:paging]['total']
   end
 
-  # Parse the response to get the desired courses data
-  puts "\nCourses from #{course_start_num} to "\
-       "#{course_start_num + batch_size - 1}:"
-  count = course_start_num
-  coursera_response[:elements].each do |element|
-    course_type = element['courseType']
-    course_id = element['id']
-    course_slug = element['slug']
-    course_name = element['name']
-
-    puts "Course #{count}: #{course_name}"
-    puts "\tType: #{course_type}"
-    puts "\tID: #{course_id}"
-    puts "\tSlug: #{course_slug}"
-    puts
-
-    count += 1
+  def query_a_batch_of_courses(course_start_num, course_num_limit)
+    query_response =
+      HTTP.get(COURSERA_CATOLOG_API_URL,
+               params: { start: course_start_num,
+                         limit: course_num_limit })
+    body_str = query_response.body.to_s
+    @coursera_response[:elements] = JSON.parse(body_str)['elements']
   end
 
-  # Increase the course number to start with next batch
-  course_start_num += batch_size
+  def parse_batch_result(course_start_num)
+    count = course_start_num
+    @coursera_response[:elements].each do |element|
+      @courses[count] = {}
+      @courses[count][:course_type] = element['courseType']
+      @courses[count][:course_id] = element['id']
+      @courses[count][:course_slug] = element['slug']
+      @courses[count][:course_name] = element['name']
+      count += 1
+    end
+  end
+
+  def print_all_courses
+    puts "#{@total_course_num} courses:"
+    count = 0
+    @courses.size.times do
+      puts "Course #{count}: #{@courses[count][:course_name]}"
+      puts "\tType: #{@courses[count][:course_type]}"
+      puts "\tID: #{@courses[count][:course_id]}"
+      puts "\tSlug: #{@courses[count][:course_slug]}"
+      puts
+      count += 1
+    end
+  end
+
+  def retrieve_all_courses_info
+    course_start_num = 0
+    course_num_limit = 100
+    number_of_batches = total_course_num / BATCH_SIZE
+    number_of_batches += 1 unless (total_course_num % BATCH_SIZE).zero?
+    number_of_batches.times do
+      # Jump out the loop if we know there is not any courses left
+      break if course_start_num >= total_course_num
+      # Use the API to get courses response on Coursera
+      query_a_batch_of_courses(course_start_num, course_num_limit)
+      # Parse the response to get the desired courses data
+      parse_batch_result(course_start_num)
+      # Increase the course number to start with next batch
+      course_start_num += BATCH_SIZE
+    end
+  end
 end
-
-puts "Total number of courses on Coursera: #{total_course_num}"
