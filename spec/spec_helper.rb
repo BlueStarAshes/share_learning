f7d50feec0f741af7f533fac7b395c7bbd20606c
blureze <blureze@gmail.com>
2016-10-21 14:18:05 +0800

    record the api call

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'json'
+require 'yaml'
+require 'vcr'
+require 'webmock'
+
+require_relative '../lib/udacity_course'
+
+FIXTURES_FOLDER = 'fixtures'
+CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
+CASSETTE_FILE = 'udacity_api'
+# CREDENTIALS = YAML.load(File.read('config/credentials.yml'))
+RESULT_FILE = "#{FIXTURES_FOLDER}/udacity_api_results.json"
+UDACITY_RESULT = JSON.parse(File.read(RESULT_FILE), :symbolize_names => true)
--- spec/udacity_api_spec.rb
+++ spec/udacity_api_spec.rb
@@ -1,44 +1,53 @@
-require 'minitest/autorun'
-require 'minitest/rg'
-require 'json'
-
-require '../lib/udacity_course.rb'
-
-COURSE_DATA = JSON.parse(File.read('data/course_test_data.json'), :symbolize_names => true)
+require_relative 'spec_helper.rb'
 
 describe 'Udacity course' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+  end
+
+  before do
+    VCR.insert_cassette CASSETTE_FILE, record: :new_episodes
+
+    @udacity_api = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
+  end
+
+  after do
+    VCR.eject_cassette
+  end
+
   it 'should get json response successfully' do
     udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
     udacity.json_response.nil?.must_equal false
   end
 
   it 'should get all courses successfully' do
-    udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
-    all_courses = udacity.acquire_all_courses
+    # udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
+    all_courses = @udacity_api.acquire_all_courses
     all_courses.nil?.must_equal false
   end
 
   it 'should get course by id successfully' do
-    udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
-    course = udacity.acquire_course_by_id('cs101')
-    course.must_equal ( COURSE_DATA[:get_course_by_id] )
+    # udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
+    course = @udacity_api.acquire_course_by_id('cs101')
+    course.must_equal ( UDACITY_RESULT[:get_course_by_id] )
   end
 
   it 'should get course by title successfully' do
-    udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
-    course = udacity.acquire_course_by_title('Intro to Java Programming')
-    course.must_equal ( COURSE_DATA[:get_course_by_title] )   
+    # udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
+    course = @udacity_api.acquire_course_by_title('Intro to Java Programming')
+    course.must_equal ( UDACITY_RESULT[:get_course_by_title] )   
   end
 
   it 'should get courses by level successfully' do
-    udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
-    course = udacity.acquire_courses_by_level('beginner')
+    # udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
+    course = @udacity_api.acquire_courses_by_level('beginner')
     course.nil?.must_equal false
   end
 
   it 'should get courses by tracks successfully' do
-    udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
-    course = udacity.acquire_courses_by_tracks('Data Science')
+    # udacity = Udacity::UdacityCourse.new(Udacity::UdacityAPI.new)
+    course = @udacity_api.acquire_courses_by_tracks('Data Science')
     course.nil?.must_equal false
   end
 end
