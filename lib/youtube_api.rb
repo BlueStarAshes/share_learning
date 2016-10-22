d9b6c5d4e50da2bb96fbb12c68a12f664ab40f18
ashley <ashley830204@gmail.com>
2016-10-23 00:29:58 +0800

    second refactoring of youtube api

--- lib/youtube_api.rb
+++ lib/youtube_api.rb
@@ -1,51 +1,38 @@
 require 'http'
 require 'json'
 
-class YouTubeAPI
-  @@DEVELOPER_KEY = 'Your_API_Key' #API key from the APIs & auth > Credentials (Google Developers Console)
-  @@maxResults =5 #number of videos that should be returned 
+module YouTube
+  #using YouTube API to get playlists
+  class YouTubeAPI
+    maxResults = 5 #number of videos that should be returned 
+    YouTube_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=playlist&order=relevance&maxResults=' + maxResults.to_s 
 
-  #Identify YouTube API key
-  def valid_APIkey
-  	access_response = 
-  	  HTTP.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=' + \
-  	  	@@maxResults.to_s + '&key=' + @@DEVELOPER_KEY)
-  	response = JSON.parse(access_response)
-  	begin
-  	  response['error'].each do |item|
-  		return item[1][0]
-  	  end
-  	rescue
-  	  return {"reason" => 'true'}
-  	end
-  end
-
-  #Retrieve the search results and get information of videos 
-  def get_videos(keyword)
-    search_response =
-      HTTP.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=' + \
-      	@@maxResults.to_s + '&order=relevance&q=' + keyword + '&type=video&key=' + @@DEVELOPER_KEY)
-    json_response = JSON.parse(search_response)
-
-    results = []
-    json_response['items'].each do |video|
-      video_title = video['snippet']['title']
-      video_des = video['snippet']['description']
-      video_url = 'https://www.youtube.com/watch?v=' + video['id']['videoId']
-      results.push({'title' => video_title, 'videoId' => video['id']['videoId']})
-      # puts "Course: #{video_title}" 
-      # puts "Description: #{video_des}"
-      # puts "URL: #{video_url}"
-      # puts
+    def initialize(api_key:, keyword:)
+      @api_key = api_key
+      @keyword = keyword
     end
-    return results
+
+    #Retrieve the search results and get information of playlists 
+    def get_playlist
+      return @info if @info
+      search_response =
+        HTTP.get(YouTube_URL + '&q=' + @keyword.split().join("+") + '&key=' + @api_key)
+      json_response = JSON.parse(search_response)
+
+      results = []
+      json_response['items'].each do |playlist|
+        title = playlist['snippet']['title']
+        des = playlist['snippet']['description']
+        image = playlist['snippet']['thumbnails']['high']['url']
+        playlistId = playlist['id']['playlistId']
+        url = 'https://www.youtube.com/channel/' + playlistId
+        results.push({'title' => title, 'description' => des, 'image' => image, 'url' => url})
+        # puts "Playlist: #{title}" 
+        # puts "Description: #{des}"
+        # puts "URL: #{url}"
+        # puts
+      end
+      @info = results
+    end
   end
 end
-
-# Prompt user to input the keyword of course 
-# puts "Input the keyword for course name or instructor's name: "
-# keyword = (gets.chomp).split().join("+")
-# YouTubeAPI.new.get_videos(keyword)
-#YouTubeAPI.new.valid_APIkey
-
-#https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=relevance&q=google&type=video&key=
