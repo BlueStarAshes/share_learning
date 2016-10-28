b839a03aeba46fb1d4c4531050dd333f61392c45
blureze <blureze@gmail.com>
2016-10-28 16:39:16 +0800

    add executable udacity

--- bin/udacity.rb
+++ bin/udacity.rb
@@ -1,21 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), *%w(.. lib))
-require 'Udacity'
+require 'Share_learning'
 
-group_id = ARGV[0] || ENV['FB_GROUP_ID']
-unless group_id
-  puts 'USAGE: facegroup [group_id]'
+course_id = ARGV[0]
+unless course_id
+  puts 'USAGE: udacity [course_id]'
   exit(1)
 end
 
-group = FaceGroup::Group.find(id: group_id)
+course = Udacity::UdacityCourse.find().acquire_course_by_id(course_id)
 
-puts group.name
-puts Array.new(group.name.length) { '-' }.join
-group.feed.postings.first(3).each.with_index do |post, index|
-  print "#{index + 1}: "
-  puts post.message ? post.message : '(blank)'
-  puts "Attached: #{post.attachment.url}" if post.attachment
-  puts
-end
+puts 'title: ' + course[:title]
+puts 'introduction: ' + course[:intro]
+puts 'link: ' + course[:link]
