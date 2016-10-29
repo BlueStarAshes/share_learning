87510bd8cc768063fcb090b006bc3e81f0e44ddf
ashley <ashley830204@gmail.com>
2016-10-30 02:59:39 +0800

    load data into ENV

--- spec/youtube_api_spec_helper.rb
+++ spec/youtube_api_spec_helper.rb
@@ -1,20 +1,21 @@
 require 'simplecov'
 SimpleCov.start
 
-require 'minitest/autorun' 
-require 'minitest/rg' 
+require 'minitest/autorun'
+require 'minitest/rg'
 require 'yaml'
 require 'json'
 require 'vcr'
 require 'webmock'
 
-require_relative '../lib/youtube_playlist'
+require_relative '../lib/share_learning/youtube_api.rb'
+require_relative '../lib/share_learning/youtube_playlist.rb'
 
-FIXTURES_FOLDER = 'fixtures' 
-CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes" 
-CASSETTE_FILE = 'youtube_api'
+FIXTURES_FOLDER = 'spec/fixtures'.freeze
+CASSETTES_FOLDER = '#{FIXTURES_FOLDER}/cassettes'.freeze
+CASSETTE_FILE = 'youtube_api'.freeze
 
-# CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
+# read credentials from a Yaml file into environment variables
 if File.file?('config/credentials.yml')
   CREDENTIALS = YAML.load(File.read('config/credentials.yml'))
   ENV['YOUTUBE_API_KEY'] = CREDENTIALS[:api_key]
