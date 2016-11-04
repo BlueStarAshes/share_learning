2bc31d83431545acd71f2967fc956243b472003c
hsing-yu, Chen <andy19933@gmail.com>
2016-11-04 20:29:52 +0800

    Updated coursera_api_spec to test the search method in coursera_courses class

--- spec/coursera_api_spec.rb
+++ spec/coursera_api_spec.rb
@@ -24,4 +24,25 @@ describe 'Coursera API specifications' do
     coursera_courses = Coursera::CourseraCourses.find
     coursera_courses.courses.size.must_equal coursera_courses.total_course_num
   end
+
+  it 'should be able to search related courses with the given keyword' do
+    keyword = 'machine learning'
+
+    matched_courses =
+      Coursera::CourseraCourses.find.search_courses(:all, keyword)
+    matched_courses.count.must_be :>, 0
+
+    matched_courses_by_title =
+      Coursera::CourseraCourses.find.search_courses(:course_name, keyword)
+    matched_courses_by_title.count.must_be :>, 0
+
+    matched_courses_by_description =
+      Coursera::CourseraCourses.find.search_courses(:description, keyword)
+    matched_courses_by_description.count.must_be :>, 0
+
+    matched_courses.count.must_be :>=, matched_courses_by_title.count
+    matched_courses.count.must_be :>=, matched_courses_by_description.count
+    sum = matched_courses_by_title.count + matched_courses_by_description.count
+    matched_courses.count.must_be :<=, sum
+  end
 end
