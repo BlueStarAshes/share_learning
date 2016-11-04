3bce2575b2d6fd92035195399d68a72a15bc3590
blureze <blureze@gmail.com>
2016-11-04 21:43:53 +0800

    refactoring udacity_course

--- lib/Share_learning/udacity_course.rb
+++ lib/Share_learning/udacity_course.rb
@@ -48,19 +48,24 @@ module Udacity
       end
     end
 
+    def substring?(title, summary, keyword)
+      ((title.include? keyword) || (summary.include? keyword))
+    end
+
     def acquire_courses_by_keywords(keyword)
       course_array = []
-      value = keyword.downcase
       @json_response['courses'].each do |course|
-        next unless course['title'].downcase.include? value or course['summary'].downcase.include? value
+        next unless substring?(course['title'].downcase, \
+                               course['summary'].downcase, keyword.downcase)
+
         h = create_hash(course['title'], course['summary'], \
                         course['homepage'], course['image'])
         course_array.push(h)
       end
-      return 'no courses found' if course_array.empty?
-      course_array  # return course_array if it is not empty
-    end
 
+      return 'no courses found' if course_array.empty?
+      course_array # return course_array if it is not empty
+    end
 
     # # get courses by skill levels ('', 'beginner', 'intermediate', 'advanced')
     # def acquire_courses_by_level(level)
@@ -82,7 +87,7 @@ module Udacity
     #   end
     # end
 
-    def self.find()
+    def self.find
       course_data = UdacityAPI.acquire_json_response
       total_course_num = UdacityAPI.total_course_num
       new(UdacityAPI, course_data, total_course_num)
